315a421cec235daadfd0384192bf7d894396f643
louis <lothmas@live.com>
2018-05-04 15:14:52 +0200

    configuring for cpanel deployment

--- src/main/java/com/iqa/Application.java
+++ src/main/java/com/iqa/Application.java
@@ -1,11 +1,19 @@
 package com.iqa;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
